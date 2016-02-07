1faee6d602c8b7edc434f9363046786e410304d8
Ronan Berder <hunvreus@gmail.com>
2016-02-07 02:34:01 -0500

    Adding the animation

--- monkey.js
+++ monkey.js
@@ -10,30 +10,43 @@ $(function() {
   }
 
   $('body').mousemove(function(e){
-    var x = e.pageX;
-    var y = e.pageY;
-    console.log('move');
+      var x = e.pageX;
+      var y = e.pageY;
+      console.log('move');
 
-    var myAngle = diff(x, y) + 180;
-    var rotationValue = 'rotate(' + myAngle + 'deg)';
-    $('#hongbao').css({
-        '-moz-transform': rotationValue,
-        '-webkit-transform': rotationValue,
-        '-o-transform': rotationValue,
-        '-ms-transform': rotationValue,
-        'transform': rotationValue
-    });
-});
+      var myAngle = diff(x, y) + 180;
+      var rotationValue = 'rotate(' + myAngle + 'deg)';
+      $('#hongbao').css({
+          '-moz-transform': rotationValue,
+          '-webkit-transform': rotationValue,
+          '-o-transform': rotationValue,
+          '-ms-transform': rotationValue,
+          'transform': rotationValue
+      });
+  });
+
+  window.setInterval(function(){
+    if ($('#hongbao').hasClass('shoot') || !shooting) {
+      $('#hongbao').removeClass('shoot');
+    }
+    else {
+      $('#hongbao').addClass('shoot');
+    }
+  }, 400);
+
+  var shooting = false;
 
   // Changing the monkey's expression on click
   $('#frown').hide();
   $('body')
     .mousedown(function () {
+      shooting = true;
       $('#face').hide();
       $('#frown').show();
       $('#hongbao').addClass('shoot');
     })
     .mouseup(function () {
+      shooting = false;
       $('#frown').hide();
       $('#face').show();
     });
